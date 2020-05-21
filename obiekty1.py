0074c29de0aafe24f30446f521301ee82879b3a3
JuliaSidor811 <64087918+JuliaSidor811@users.noreply.github.com>
2020-05-21 15:30:30 +0200

    modul 7 zadanie 1 poprawione

--- obiekty1.py
+++ obiekty1.py
@@ -1,16 +1,18 @@
 from faker import Faker
 
+
 class BaseContact:
-    def __init__(self,name:str,last_name:str,phone:int,email:str):
+    def __init__(self, name: str, last_name: str, phone: int, email: str):
         self.name = name
         self.last_name = last_name
         self.phone = phone
         self.email = email
+
     def __str__(self):
         return f'Name: {self.name} Last Name: {self.last_name} Email: {self.email}'
 
     def contact(self):
-        return f'I dial the number: {self.phone} and call {self.name} {self.last_name}'
+        print(f'I dial the number: {self.phone} and call {self.name} {self.last_name}')
 
     @property
     def label_lenght(self):
@@ -18,8 +20,9 @@ class BaseContact:
 
 
 class BusinessCard(BaseContact):
-    def __init__(self,occupation:str,company_name:str,business_phone:int,*args,**kwargs):
-        super().__init__(*args,**kwargs)
+    def __init__(self, name: str, last_name: str, phone: int, email: str, occupation: str, company_name: str,
+                 business_phone: int):
+        super().__init__(name, last_name, phone, email)
         self.occupation = occupation
         self.company_name = company_name
         self.business_phone = business_phone
@@ -28,66 +31,62 @@ class BusinessCard(BaseContact):
         return f'I dial the number: {self.business_phone} and call {self.name} {self.last_name}'
 
 
-def create_contacts(card_type,quantity:int):
+def create_contacts(card_type, quantity: int):
     faker = Faker()
     person_list = []
-    for number in range(0,quantity):
-        if card_type == BaseContact:
-            person_list.append(BaseContact(faker.first_name(),faker.last_name(),faker.phone_number(),faker.email()))
+    for number in range(quantity):
+        if card_type == BusinessCard:
+            person_list.append(BusinessCard(name=faker.first_name(),
+                                            last_name=faker.last_name(),
+                                            phone=faker.phone_number(),
+                                            email=faker.email(),
+                                            occupation=faker.job(),
+                                            company_name=faker.company(),
+                                            business_phone=faker.phone_number(),
+                                            ))
+        elif card_type == BaseContact:
+            person_list.append(BaseContact(name=faker.first_name(),
+                                           last_name=faker.last_name(),
+                                           phone=faker.phone_number(),
+                                           email=faker.email()))
         else:
-            person_list.append(BusinessCard(faker.job(),faker.company(),faker.phone_number(),
-                                faker.first_name(),faker.last_name(),faker.phone_number(),faker.email()))
+            raise TypeError
 
     return person_list
 
 
-
 def main():
+    base_contacts = create_contacts(BaseContact, 5)
+    businnes_contacts = create_contacts(BusinessCard, 3)
+    contacts_all = base_contacts + businnes_contacts
 
-    base_contacts = create_contacts(BaseContact,5)
-    businnes_contacts = create_contacts(BusinessCard,3)
+    print("ALL CONTACTS")
 
-    print('PODSTAWOWE\n')
-    for person in base_contacts:
+    for person in contacts_all:
         print(person)
 
-    print('\nBIZNESOWE')
+    # SORT
+    sorted_by_name = sorted(contacts_all, key=lambda human: human.name)
+    sorted_by_lastname = sorted(contacts_all, key=lambda human: human.last_name)
+    sorted_by_email = sorted(contacts_all, key=lambda human: human.email)
 
-    for person in businnes_contacts:
-        print(person)
-
-    #sortowanie
-    sorted_by_name = sorted(base_contacts, key=lambda human: human.name)
-    sorted_by_lastname = sorted(businnes_contacts, key=lambda human: human.last_name)
-    sorted_by_email = sorted(base_contacts, key=lambda human: human.email)
-
-    print('\nSORTED PERSON BY NAME\n')
+    print('SORTED PERSON BY NAME')
     for person_name in sorted_by_name:
         print(person_name)
-    print('\nSORTED PERSON BY LAST NAME\n')
+    print('SORTED PERSON BY LAST NAME')
     for person_lastname in sorted_by_lastname:
         print(person_lastname)
-    print('\nSORTED BY EMAIL\n')
+    print('SORTED BY EMAIL')
     for person_email in sorted_by_email:
         print(person_email)
 
-    #dlugosc pelnych imion w podstawowych
-    for base in base_contacts:
-        print(base.label_lenght)
+    # FULL NAMES LEN
+    for person in contacts_all:
+        print(person.label_lenght)
 
-    #dlugosc pelnych imion w biznesowych
-    for businnes in businnes_contacts:
-        print(businnes.label_lenght)
-
-    #metoda contact
-    print(base_contacts[2].contact())
-    print(businnes_contacts[1].contact())
-
-
+    # CONTACT
+    print(contacts_all[2].contact())
 
 
 if __name__ == '__main__':
     main()
-
-
-
